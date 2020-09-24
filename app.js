ba4a61bd66a7cd912342180468e8b5e58e149208
Natalia Titova <bnmlucky@gmail.com>
2020-09-24 13:57:25 -0400

    adding views and redirection

--- app.js
+++ app.js
@@ -1,13 +1,42 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
+
 app.get('/', (req, res) => {
-    res.send('<h1>I love Treehouse!</h1>');
+    const name = req.cookies.username;
+    if (name) {
+        res.render('index', { name });
+    } else {
+        res.redirect('/hello');
+    }
 });
 
+app.get('/cards', (req, res) => {
+    res.render('card', { prompt: "Who is buried in Grant's tomb?" });
+});
+
+// /sandbox
+// First Name | Last Name
+
 app.get('/hello', (req, res) => {
-    res.send('<h1>Hello, JavaScript Developer!</h1>');
+    const name = req.cookies.username;
+    if (name) {
+        res.redirect('/');
+    } else {
+        res.render('hello');
+    }
+});
+
+app.post('/hello', (req, res) => {
+    res.cookie('username', req.body.username);
+    res.redirect('/');
 });
 
 app.listen(3000, () => {
